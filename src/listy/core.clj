d4e398ce8b517bcb4af9d802e90ba4f2924f9ffb
halogenandtoast <halogenandtoast@gmail.com>
2013-11-15 14:04:52 -0500

    Hello world

--- src/listy/core.clj
+++ src/listy/core.clj
@@ -1,6 +1,10 @@
-(ns listy.core)
+(ns listy.core
+  (:use [compojure.core :only (defroutes GET)]
+        [ring.adapter.jetty :as ring]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defroutes routes
+  (GET "/" [] "<h2>Hello from Listy</h2>"))
+
+(defn -main []
+  (run-jetty #'routes {:port 8080 :join? false}))
